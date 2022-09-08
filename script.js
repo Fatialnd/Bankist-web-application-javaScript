8929076d2790ce1fd5e8dc10f7fcc9cfd6a2fb75
Fatima <75587225+Fatialnd@users.noreply.github.com>
2022-09-08 20:17:47 +0200

    loading images

--- script.js
+++ script.js
@@ -107,3 +107,20 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+const imgTarget = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserver(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTarget.forEach(img => imgObserver.observe(img));
